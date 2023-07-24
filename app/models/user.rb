3e23680783f557ab750542273aca4f63139d560b
rails udemy course <syeda.sara@welldev.io>
2023-07-24 14:58:32 +0600

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    validates :username, presence: true, 
+              length: {minimum: 3, maximum: 25}, 
+              uniqueness: {case_sensitive: false}
+    validates :email, presence: true ,
+              length: { maximum: 103 },
+              uniqueness: {case_sensitive: false},
+              format: { with: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/i , message: "Not a valid email address"}
+
 end
