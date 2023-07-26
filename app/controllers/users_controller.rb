c988d822957764f3e8a3b069526d32c8866825dc
rails udemy course <syeda.sara@welldev.io>
2023-07-26 13:46:13 +0600

    add, edit, delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+    def index 
+        @user = User.all
+    end
+
     def new
         @user = User.new
+    end
+
+    def show 
+        @user = User.find(params[:id])
     end
 
     def create 
@@ -8,11 +16,31 @@ class UsersController < ApplicationController
        
         if @user.save
             flash[:success] = "Welcome to the alpha blog #{@user.username}"
-            redirect_to articles_path
+            redirect_to users_path
         else
             render 'new' , status: :unprocessable_entity
         end
     end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render 'edit', status: unprocessable_entity
+        end
+    end
+    
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_path, status: :see_other
+    end
+
     private
     def user_params
      params.require(:user).permit(:username, :email, :password)
